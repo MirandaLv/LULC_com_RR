9afc272e11b3b2f9ad7b77e2960ffeb95ef96b7f
MirandaLv <zhonghui.lv@gmail.com>
2024-12-03 11:10:11 -0500

    modify code so can be called on terminal

--- scripts/geotiff_processing.py
+++ scripts/geotiff_processing.py
@@ -4,19 +4,29 @@ import geopandas as gpd
 import os
 import rasterio
 from shapely.geometry import box
+import argparse
 
 
-def main(input_path, output_path):
+def main():
+
+    parser = argparse.ArgumentParser(description='Description of your script.')
+    parser.add_argument('input_file', type=str, help='Path to the input shapefile file.')
+    parser.add_argument('geotiff_dir', type=str, help='Path to the folder of NAIP images.')
+    parser.add_argument('--output_file', type=str, help='Path to the output file.', default='output.shp')
+    args = parser.parse_args()
+
     # Check if input and output directories exist
-    if not os.path.exists(input_path):
-        print(f"Error: Input directory '{input_path}' does not exist.")
+    if not os.path.exists(args.input_file):
+        print(f"Error: Input directory '{args.input_path}' does not exist.")
         return
 
     # buildings = os.path.join(working_dir, 'data/processed/building_tag.shp')
 
-    gdf = gpd.read_file(input_path)
+    gdf = gpd.read_file(args.input_file)
 
     # Loop through each GeoTIFF file to find matching polygons
+    geotiff_files = [os.path.join(args.geotiff_dir, i) for i in os.listdir(args.geotiff_dir) if i.endswith('.tif')]
+
     for geotiff_path in geotiff_files:
         # Open the GeoTIFF file and get its bounds
         with rasterio.open(geotiff_path) as src:
@@ -30,14 +40,16 @@ def main(input_path, output_path):
         gdf.loc[matching_buildings.index, "geotiff"] = geotiff_name
 
     gdf = gdf[gdf['geotiff'].notnull()]  # remove polygons that has no geotiff data coverage
-    gdf.to_file(output_path)
+    gdf.to_file(args.output_file)
 
 
 if __name__ == "__main__":
-    working_dir = os.path.abspath('../')
-    naip_dir = os.path.join(working_dir, 'data/NAIP/Mathew')
-    geotiff_files = [os.path.join(naip_dir, i) for i in os.listdir(naip_dir) if i.endswith('.tif')]
-    inpath = os.path.join(working_dir, 'data/buildingfootprint/Mathew/Mathew_buildingfootprint.shp')
-    outpath = os.path.join(working_dir, 'data/processed/Mathew/building_mathew_tiff.shp')
-    main(inpath, outpath)
+    main()
+
+"""
+in: "/Users/mirandalv/Documents/projects/github/LULC_com_RR/data/buildingfootprint/Mathew/Mathew_buildingfootprint.shp"
+dir: "/Users/mirandalv/Documents/projects/github/LULC_com_RR/data/NAIP/Mathew"
+out: "/Users/mirandalv/Documents/projects/github/LULC_com_RR/data/processed/Mathew/building_mathew_tiff_delete.shp"
+"""
+
 
